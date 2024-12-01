33bf2c1945ca2a47f406974f917a9b176f0644c1
zlonce <zlon@outlook.kr>
2024-12-02 04:41:29 +0900

    adding map, upload page

--- lib/models/common/photo.dart
+++ lib/models/common/photo.dart
@@ -13,7 +13,7 @@ class Photo {
   final int likes;
   final int views;
   final String? tag;
-  final User? user;  // API 응답에 따라 optional로 변경
+  final User? user; // API 응답에 따라 optional로 변경
 
   Photo({
     required this.photoId,
@@ -66,4 +66,35 @@ class Photo {
       if (user != null) 'user': user!.toJson(),
     };
   }
+}
+
+//photo랑 어떻게 나눠야 할지 몰라서 그냥 하나 넣었어요..
+class ImageUploadData {
+  final int userId;
+  final double lat;
+  final double lng;
+  final String location;
+  final int registerTime;
+  final bool frameActive;
+  final bool sharedActive;
+
+  ImageUploadData({
+    required this.userId,
+    required this.lat,
+    required this.lng,
+    required this.location,
+    required this.registerTime,
+    required this.frameActive,
+    required this.sharedActive,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'lat': lat,
+        'lng': lng,
+        'location': location,
+        'registerTime': registerTime,
+        'frameActive': frameActive,
+        'sharedActive': sharedActive,
+      };
 }
